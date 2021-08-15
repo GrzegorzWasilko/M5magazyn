d5d615853fe73ac854de82f01be99cfacffdd083
Grzegorz Wasilko <wasilko1@o2.pl>
2021-08-15 09:52:04 +0200

    FIrst exit

---
+++ Magazyn_Mod5.py
@@ -0,0 +1,10 @@
+#Magazyn Moduł 5 
+#1commit f exit program ma ie włczyć 
+import sys
+
+in=input("na razie wpisz tylko exit")
+
+while in == 'exit'
+    print("wpisałeś coś innego niż exit ")
+print('by by')
+exit(0)
